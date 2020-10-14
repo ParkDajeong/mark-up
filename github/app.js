f8fd4e2892d11ca3def398ab14927ec0dc7ddba6
ParkDajeong <sobeast980@gmail.com>
2020-10-14 16:43:37 +0900

    깃허브 마크업

---
+++ github/app.js
@@ -0,0 +1,4 @@
+const btnMenu = document.querySelector(".btn-menu");
+const headerBottom = document.querySelector(".gnb-wrapper");
+
+btnMenu.addEventListener("click", () => headerBottom.classList.toggle("hidden"))
