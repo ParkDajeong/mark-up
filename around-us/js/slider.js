dad709cbc55a9bffd1bb3e4646d392f900ce888e
ParkDajeong <sobeast980@gmail.com>
2020-10-27 13:12:18 +0900

    social swiper 구현

--- around-us/js/slider.js
+++ around-us/js/slider.js
@@ -22,21 +22,46 @@ $(function() {
     slidesToScroll: 1,
     speed: 500,
     arrows: true,
-    prevArrow: "<button class='btn-icon slider__btn-arrow slider__btn-prev'>Previous</button>",
-    nextArrow: "<button class='btn-icon slider__btn-arrow slider__btn-next'>Next</button>",
+    prevArrow: "<button class='btn-icon btn-icon--prev slider__btn-arrow'>Previous</button>",
+    nextArrow: "<button class='btn-icon btn-icon--next slider__btn-arrow'>Next</button>",
     swipe: true,
   });
 
   // Content - SNS
-  // $(".content-sns .content__slider").slick({
-  //   slide: "div",
-  //   infinite: true,
-  //   slidesToShow: 6, 
-  //   slidesToScroll: 4,
-  //   speed: 500,
-  //   arrows: true,
-  //   prevArrow: "<button class='btn-icon slider__btn-arrow slider__btn-prev'>Previous</button>",
-  //   nextArrow: "<button class='btn-icon slider__btn-arrow slider__btn-next'>Next</button>",
-  //   swipeToSlide: true,
-  // });
+  const snsSwiper = $(".content__swiper");
+  let isMouseDown = false;
+  let startX = 0;
+  let scrollLeft = 0;
+
+  snsSwiper.mousedown(function(e) {
+    isMouseDown = true;
+    startX = e.pageX - snsSwiper.offset().left;
+    scrollLeft = snsSwiper.scrollLeft();
+  });
+
+  snsSwiper.mousemove(function(e) {
+    if(!isMouseDown) return;
+    e.preventDefault();
+
+    const x = e.pageX - snsSwiper.offset().left;
+    const movement = (x - startX) * 1.2;
+    snsSwiper.scrollLeft(scrollLeft - movement);
+    snsSwiper.find("a").on("click", function(e) {
+      e.preventDefault();
+    });
+  });
+
+  snsSwiper.mouseup(function() {
+    isMouseDown = false;
+    setTimeout(function() {
+      snsSwiper.find("a").off("click");
+    }, 500);
+  });
+
+  snsSwiper.mouseleave(function() {
+    isMouseDown = false;
+  })
+
+  // Swiper Arrow Button
+
 });
