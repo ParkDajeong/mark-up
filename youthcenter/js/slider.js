f0bf6e42e68f4754a618dc638729811e00c3203f
ParkDajeong <sobeast980@gmail.com>
2020-11-06 13:01:38 +0900

    Content - 청년정책 리스트 구현

--- youthcenter/js/slider.js
+++ youthcenter/js/slider.js
@@ -1,4 +1,5 @@
 $(function() {
+  // ============= 메인 배너 =============
   $(".main-content__banner").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -6,4 +7,37 @@ $(function() {
     autoplaySpeed: 5000,
     draggable: false,
   });
+
+  // ============= 정책 리스트 =============
+  const navList = ["취업지원", "창업지원", "주거·금융", "생활·복지", "정책참여"];
+
+  $(".main-content__policy").slick({
+    slidesToScroll: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: true,
+    dots: true,
+    appendDots: $(".main-content__policy-nav"),
+    customPaging:  function(slider, i) {
+      return "<button class='slick-dot' type='button'>" + navList[i] + "</button>"
+    },
+    responsive: [
+      {
+        breakpoint: 1070,
+        settings: {
+          arrows: false,
+        }
+      }
+    ],
+  });
+
+  // $(".main-content__btn-policy").click(function() {
+  //   const slideNo = $(this).index();
+
+  //   $(".main-content__btn-policy").removeClass("active");
+  //   $(this).addClass("active");
+  //   $(".main-content__policy").slick("slickGoTo", slideNo);
+  // });
+
+  // $(".main-content__btn-policy").first().addClass("active");
 });
