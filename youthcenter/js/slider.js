151ecbf7c08e5ba55a56f1fb8ebd8dca1f68a0bb
ParkDajeong <sobeast980@gmail.com>
2020-11-06 16:48:48 +0900

    Content - AD Banner 구현

--- youthcenter/js/slider.js
+++ youthcenter/js/slider.js
@@ -1,8 +1,17 @@
 $(function() {
+  // ============= Slide Play & Pause =============
+  $(".slider-dots-wrap .btn-play").click(function() {
+    const slider = "." + $(this).data("slider");
+    $(slider).slick("slickPlay");
+  });
+
+  $(".slider-dots-wrap .btn-pause").click(function() {
+    const slider = "." + $(this).data("slider");
+    $(slider).slick("slickPause");
+  });
+
   // ============= 메인 배너 =============
   $(".main-content__banner").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
     draggable: false,
@@ -12,14 +21,12 @@ $(function() {
   const navList = ["취업지원", "창업지원", "주거·금융", "생활·복지", "정책참여"];
 
   $(".main-content__policy").slick({
-    slidesToScroll: 1,
-    slidesToScroll: 1,
     infinite: false,
     arrows: true,
     dots: true,
     appendDots: $(".main-content__policy-nav"),
     customPaging:  function(slider, i) {
-      return "<button class='slick-dot' type='button'>" + navList[i] + "</button>"
+      return "<button class='nav-item' type='button'>" + navList[i] + "</button>"
     },
     responsive: [
       {
@@ -30,14 +37,16 @@ $(function() {
       }
     ],
   });
-
-  // $(".main-content__btn-policy").click(function() {
-  //   const slideNo = $(this).index();
-
-  //   $(".main-content__btn-policy").removeClass("active");
-  //   $(this).addClass("active");
-  //   $(".main-content__policy").slick("slickGoTo", slideNo);
-  // });
-
-  // $(".main-content__btn-policy").first().addClass("active");
+  
+  // ============= AD 배너 =============
+  $(".main-content__ad-banner").slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: true,
+    appendDots: $(".slider-dot-list"),
+    customPaging: function(slider, i) {
+      return "<button class='slick-dot' type='button'><span class='sr-only'>" + i + "번 슬라이드로 이동</span></button>"
+    }
+  });
 });
