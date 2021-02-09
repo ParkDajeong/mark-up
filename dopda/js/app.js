a0b8b378fb9135a2ace44bbcb4b75807d6c690ac
ParkDajeong <sobeast980@gmail.com>
2021-02-09 17:51:43 +0900

    fixheader 구현

--- dopda/js/app.js
+++ dopda/js/app.js
@@ -1,4 +1,15 @@
 $(function() {
+  // Fix Header
+  $(window).on("resize scroll", function() {
+    const scroll = $(this).scrollTop();
+    if(scroll >= 100) {
+      $(".main-header").css({"background-color": "#fff"});
+    } else {
+      $(".main-header").css({"background-color": "transparent"});
+    }
+  });
+
+  // Visual Slide
   $(".visual .slide").slick({
     dots: true,
     arrows: false,
