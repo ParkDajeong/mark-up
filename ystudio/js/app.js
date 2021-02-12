14eecf9e3a86137f29139e40a57e6313f1c0ace0
ParkDajeong <sobeast980@gmail.com>
2021-02-12 21:02:13 +0900

    visual slide 애니메이션 구현

--- ystudio/js/app.js
+++ ystudio/js/app.js
@@ -14,4 +14,11 @@ $(function() {
   $(".btn-menu-open, .btn-menu-close").on("click", function() {
     $(".gnb").toggleClass("on");
   });
+
+  // Slick Slide
+  $(".slide").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+  });
 });
