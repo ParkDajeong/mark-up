6f513b7a2d93f879fdad0cd8ea8e3eb76f0b599f
ParkDajeong <sobeast980@gmail.com>
2021-02-21 09:23:53 +0900

    Section Banner 레이아웃 구현

--- fuseseoul/js/app.js
+++ fuseseoul/js/app.js
@@ -12,4 +12,21 @@ $(function() {
       <i class="fas fa-arrow-right"></i>
     </button>`,
   });
+
+  $(".slide--multiple").slick({
+    speed: 800,
+    autoplay: false,
+    autoplaySpeed: 500,
+    slidesToShow : 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    prevArrow: `<button type='button' class='slick-arrow slick-prev'>
+      <span class='blind'>이전 슬라이드로</span>
+      <i class="fas fa-angle-left"></i>
+    </button>`,
+    nextArrow: `<button type='button' class='slick-arrow slick-next'>
+      <span class='blind'>다음 슬라이드로</span>
+      <i class="fas fa-angle-right"></i>
+    </button>`,
+  });
 });
