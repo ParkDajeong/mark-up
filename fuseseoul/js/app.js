3055e1b079a09e522840a92037d5a18d181e92ba
ParkDajeong <sobeast980@gmail.com>
2021-02-21 10:43:47 +0900

    Section recommendation 레이아웃 구현

--- fuseseoul/js/app.js
+++ fuseseoul/js/app.js
@@ -13,13 +13,29 @@ $(function() {
     </button>`,
   });
 
-  $(".slide--multiple").slick({
+  $(".slide--multiple-center").slick({
     speed: 800,
     autoplay: true,
     autoplaySpeed: 3000,
     slidesToShow : 3,
     slidesToScroll: 1,
     centerMode: true,
+    prevArrow: `<button type='button' class='slick-arrow slick-prev'>
+      <span class='blind'>이전 슬라이드로</span>
+      <i class="fas fa-angle-left"></i>
+    </button>`,
+    nextArrow: `<button type='button' class='slick-arrow slick-next'>
+      <span class='blind'>다음 슬라이드로</span>
+      <i class="fas fa-angle-right"></i>
+    </button>`,
+  });
+
+  $(".slide--multiple").slick({
+    speed: 800,
+    autoplay: false,
+    infinite: false,
+    slidesToShow : 4,
+    slidesToScroll: 4,
     prevArrow: `<button type='button' class='slick-arrow slick-prev'>
       <span class='blind'>이전 슬라이드로</span>
       <i class="fas fa-angle-left"></i>
